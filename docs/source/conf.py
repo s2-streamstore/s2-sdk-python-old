36e7509e08d1c13355e5772fc9e27394683d603b
quettabit <27509167+quettabit@users.noreply.github.com>
2026-04-06 23:37:59 -0600

    release `5.0.1` (#111)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ from datetime import date
 
 project = "streamstore"
 copyright = f"{date.today().year}, Bandar Systems Inc"
-release = "5.0.0"
+release = "5.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
